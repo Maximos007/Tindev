d07f337baa29be3fba65d1a18e89120e125b7596
Maximos007 <waslleymax@gmail.com>
2021-02-10 13:28:45 -0300

    App

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const routes = require('./routes');
+
+const server = express();
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-gtaac.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+//GET, POST, PUT, DELETE
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
